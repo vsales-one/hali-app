428c7341b8fc17da574f94b88e5c9403854a4901
Trung vu <francisdinhtrung@gmail.com>
2019-12-07 08:41:23 +0700

    - fix error my profile

--- src/hali/lib/my_profile/my_profile_event.dart
+++ src/hali/lib/my_profile/my_profile_event.dart
@@ -1,14 +1,13 @@
 import 'dart:async';
 import 'dart:developer' as developer;
 
-import 'package:new_instagramm/my_profile/index.dart';
+import 'package:hali/my_profile/index.dart';
 import 'package:meta/meta.dart';
 
 @immutable
 abstract class MyProfileEvent {
   Future<MyProfileState> applyAsync(
       {MyProfileState currentState, MyProfileBloc bloc});
-  final MyProfileRepository _myProfileRepository = MyProfileRepository();
 }
 
 class UnMyProfileEvent extends MyProfileEvent {
@@ -29,16 +28,7 @@ class LoadMyProfileEvent extends MyProfileEvent {
   @override
   Future<MyProfileState> applyAsync(
       {MyProfileState currentState, MyProfileBloc bloc}) async {
-    try {
-      if (currentState is InMyProfileState) {
-        return currentState.getNewVersion();
-      }
-      await Future.delayed(Duration(seconds: 2));
-      this._myProfileRepository.test(this.isError);
-      return InMyProfileState(0, "Hello world");
-    } catch (_, stackTrace) {
-      developer.log('$_', name: 'LoadMyProfileEvent', error: _, stackTrace: stackTrace);
-      return ErrorMyProfileState(0, _?.toString());
-    }
+
+
   }
 }
